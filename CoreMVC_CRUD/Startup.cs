8e344830a7776eab2f099264e17d8f032c96aa72
gawronCode <gawronCode@gmail.com>
2020-11-15 18:08:03 +0100

    Add DbContext and connection strings

--- CoreMVC_CRUD/Startup.cs
+++ CoreMVC_CRUD/Startup.cs
@@ -6,8 +6,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreMVC_CRUD.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreMVC_CRUD
 {
@@ -23,12 +26,24 @@ namespace CoreMVC_CRUD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(opt => opt.UseSqlServer(GetConnectionString()));
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddControllersWithViews();
+        }
+
+        private string GetConnectionString()
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("CoreMVC_CRUD_Connection");
+            builder.UserID = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            return builder.ConnectionString;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseBrowserLink();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
